bad4abc7c502bf6325c6166553265fa4f2614a92
reganbrown <regan_brown@icloud.com>
2024-09-18 17:58:29 -0300

    finish create-header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.scss";
 import Header from "./components/Header/Header";
+import VideoPlayer from "./components/VideoPlayer/VideoPlayer";
 
 function App() {
   return (
     <>
       <Header />
+      <VideoPlayer />
     </>
   );
 }
