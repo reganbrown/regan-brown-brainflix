35bcd23eef9aba85b9e7d080a7b097fb678c9a8a
reganbrown <regan_brown@icloud.com>
2024-09-23 22:06:32 -0300

    move all error handling outside of useEffect

--- src/pages/VideoDetailsPage/VideoDetailsPage.jsx
+++ src/pages/VideoDetailsPage/VideoDetailsPage.jsx
@@ -7,6 +7,28 @@ import Body from "../../components/Body/Body";
 export default function VideoDetailsPage({ apiKey }) {
   let params = useParams();
   let navigate = useNavigate();
+  let [video, setVideo] = useState(null);
+  let [videoList, setVideoList] = useState([]);
+
+  async function handleVideoID(videoID) {
+    navigate(`/id/${videoID}`);
+  }
+
+  let getVideosData = async () => {
+    try {
+      let results = await axios.get(
+        `https://unit-3-project-api-0a5620414506.herokuapp.com/videos/?api_key=${apiKey}`
+      );
+      if (params.id === undefined) {
+        getCurrentVideo(results.data[0].id);
+      } else {
+        getCurrentVideo(params.id);
+      }
+      setVideoList(results.data);
+    } catch (error) {
+      navigate("/404");
+    }
+  };
 
   const getCurrentVideo = async (id) => {
     try {
@@ -19,33 +41,8 @@ export default function VideoDetailsPage({ apiKey }) {
     }
   };
 
-  async function handleVideoID(videoID) {
-    navigate(`/id/${videoID}`);
-  }
-
-  let [video, setVideo] = useState(null);
-
-  // set video list using starting list
-  let [videoList, setVideoList] = useState([]);
-
   useEffect(() => {
-    let getData = async () => {
-      try {
-        let results = await axios.get(
-          `https://unit-3-project-api-0a5620414506.herokuapp.com/videos/?api_key=${apiKey}`
-        );
-        if (params.id === undefined) {
-          getCurrentVideo(results.data[0].id);
-        } else {
-          getCurrentVideo(params.id);
-        }
-        setVideoList(results.data);
-      } catch (error) {
-        navigate("/404");
-      }
-    };
-
-    getData();
+    getVideosData();
   }, [params]);
 
   if (video === null) {
